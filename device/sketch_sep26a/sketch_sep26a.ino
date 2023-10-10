43e0c802676a5bb61eaf2ef066e729c412dc5fff
figitalproje <146656596+figitalproje@users.noreply.github.com>
2023-10-10 06:03:40 +0300

    servo eeprom + servo ble kontrol

--- device/sketch_sep26a/sketch_sep26a.ino
+++ device/sketch_sep26a/sketch_sep26a.ino
@@ -10,20 +10,24 @@ const int PRIVATE_KEY_SIZE = 32;
 const int PUBLIC_KEY_SIZE = 32;
 const int PRIVATE_KEY_ADDR = 0;
 const int PUBLIC_KEY_ADDR = PRIVATE_KEY_ADDR + PRIVATE_KEY_SIZE;
+const int SERVO_ADDR=PUBLIC_KEY_ADDR+PUBLIC_KEY_SIZE; 
 
 uint8_t privateKey[PRIVATE_KEY_SIZE];
 uint8_t publicKey[PUBLIC_KEY_SIZE];
 String barkod;
 Servo myservo;
 
+
 BLEServer* pServer = NULL;
 BLECharacteristic* pHandshakeCharacteristic = NULL;
 BLECharacteristic* pSignatureCharacteristic = NULL;
+BLECharacteristic* pServoCharacteristic = NULL;
 bool deviceConnected = false;
 
 const char* SERVICE_UUID = "2b5dc6e0-2727-453f-b69d-7fa0f50c7705";
 const char* HANDSHAKE_CHARACTERISTIC_UUID = "0abd0e1d-b033-486d-bd6f-a9423c091146";
 const char* SIGNATURE_CHARACTERISTIC_UUID ="c029a5f5-03fe-40ff-a379-896dfa677240";
+const char* SERVO_CHARACTERISTIC_UUID ="ca9d304b-1afc-4006-94fc-89b37d127c6c";
 
 
 void HandShake() {
@@ -73,6 +77,20 @@ class SignatureCallbacks: public BLECharacteristicCallbacks {
       }
     }
 };
+class ServoCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      std::string value = pCharacteristic->getValue();
+      if (value.length() > 0) {
+        int pos = atoi(value.c_str());  // String değeri tam sayıya dönüştür
+        if (pos >= 0 && pos <= 180) {    // Servo motor için güvenli bir aralık kontrolü
+          myservo.write(pos);
+          EEPROM.write(SERVO_ADDR, pos);  // Yeni pozisyonu EEPROM'a kaydet
+          EEPROM.commit();  // Değişiklikleri EEPROM'a yaz
+        }
+      }
+    }
+};
+
 
 String publicKeyToString(uint8_t* publicKey) {
   String result;
@@ -124,6 +142,11 @@ void setup() {
   }
 
   myservo.attach(13);
+  if(EEPROM.read(SERVO_ADDR)==0xFF){
+    EEPROM.write(SERVO_ADDR,0);
+  }
+    int servoson = EEPROM.read(SERVO_ADDR);  // EEPROM'dan servoson değerini oku
+  myservo.write(servoson);  // Servo motorun pozisyonunu ayarla
   Serial.begin(9600);
   delay(1000);
 
@@ -157,6 +180,14 @@ pSignatureCharacteristic = pService->createCharacteristic(
 pSignatureCharacteristic->setCallbacks(new SignatureCallbacks());
 pSignatureCharacteristic->addDescriptor(new BLE2902());
 
+
+pServoCharacteristic =pService-> createCharacteristic(
+                      SERVO_CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                     BLECharacteristic::PROPERTY_WRITE  
+);
+pServoCharacteristic->setCallbacks(new ServoCallbacks());
+pServoCharacteristic->addDescriptor(new BLE2902());
 
   pService->start();
   BLEAdvertising *pAdvertising = pServer->getAdvertising();
